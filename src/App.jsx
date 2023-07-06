3ee05bcfdc2d166ef57c179c26cd1170fd9fbd09
Lemar Levy <lemarlevy@gmail.com>
2023-07-06 16:17:12 -0400

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import { useState } from 'react'
+
+
+
+import { useState } from 'react';
 
 function Picker() {
-const [selectedColor, setSelectedColor] = useState("")
-const Color = ({color, setSelectedColor}) => {
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const Color = ({ color, setSelectedColor }) => {
+    return (
+      <div
+        className={color}
+        onClick={() => setSelectedColor(color)}
+      ></div>
+    );
+  };
+
   return (
-    <div 
-      className={color} 
-      onClick={() => setSelectedColor(color)} 
-     ></div>
-  )}
-}
-      <div id="colors-list">{'yellow, blue ,violet'}
-      
+    <div>
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className={selectedColor}>{selectedColor}</div>
+      </div>
+
       <div id="colors-list">
-  <Color color="yelow" setSelectedColor={setSelectedColor}/>
-  <Color color="blue" setSelectedColor={setSelectedColor}/>
-  <Color color="violet" setSelectedColor={setSelectedColor}/>
-</div>
- </div>
+        <Color color="yellow" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="violet" setSelectedColor={setSelectedColor} />
+      </div>
+    </div>
+  );
+}
 
-
-
-
-
-export default App
-
-
-
+export default Picker;
